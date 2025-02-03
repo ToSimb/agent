de3c3a1b5328ea7aead89a7d2ddfb36aa9defa3c
Smirnov <Smirnov>
2025-02-03 11:04:19 +0300

    cpu

--- monitoring/cpu/cpu.py
+++ monitoring/cpu/cpu.py
@@ -4,63 +4,81 @@ import wmi
 import subprocess
 
 
-# Поменять на try
 
 def get_cpu_physical_core_count():
     """Возвращает количество физических ядер"""
-    return psutil.cpu_count(logical=False) or -1
+    try:
+        return psutil.cpu_count(logical=False)
+    except Exception as e:
+        print(f"Ошибка сбора параметра \"Количество физических ядер\": \n{e}")
+        return -1
 
 
 def get_cpu_logical_core_count():
     """Возвращает количество логических ядер"""
-    return psutil.cpu_count(logical=True) or -1
+    try:
+        return psutil.cpu_count(logical=True)
+    except Exception as e:
+        print(f"Ошибка сбора параметра \"Количество логических ядер\": \n{e}")
+        return -1
 
 
 def get_cpu_usage_per_core():
     """Возвращает загрузку каждого логического процессора"""
-    return psutil.cpu_percent(interval=1, percpu=True) or -1
+    try:
+        return psutil.cpu_percent(interval=1, percpu=True)
+    except Exception as e:
+        print(f"Ошибка сбора параметра \"Загрузка логических процессоров\": \n{e}")
+        return -1
 
 
 def get_interrupt_count():
     """Возвращает количество прерываний в системе"""
-    return psutil.cpu_stats().interrupts or -1
+    try:
+        return psutil.cpu_stats().interrupts
+    except Exception as e:
+        print(f"Ошибка сбора параметра \"Количество прерываний в системе\": \n{e}")
+        return -1
 
 
 def get_cpu_time_user():
-    """Возвращает процент времени, которое центральный
-       процессор тратит на обработку пользовательских операций"""
-    return psutil.cpu_times_percent(interval=1).user or -1
+    """Возвращает процент времени, которое центральный процессор тратит на обработку пользовательских операций"""
+    try:
+        return psutil.cpu_times_percent(interval=1).user
+    except Exception as e:
+        print(f"Ошибка сбора параметра \"Процент времени CPU на обработку пользовательских операций\": \n{e}")
+        return -1
 
 
 def get_cpu_time_system():
-    """Возвращает процент времени, которое центральный
-       процессор тратит на обработку системных операций"""
-    return psutil.cpu_times_percent(interval=1).system or -1
+    """Возвращает процент времени, которое центральный процессор тратит на обработку системных операций"""
+    try:
+        return psutil.cpu_times_percent(interval=1).system
+    except Exception as e:
+        print(f"Ошибка сбора параметра \"Процент времени CPU на обработку системных операций\": \n{e}")
+        return -1
 
 
 def get_cpu_time_irq():
-    """Возвращает процент времени, которое центральный
-       процессор тратит на обработку аппаратных прерываний"""
+    """Возвращает процент времени, которое центральный процессор тратит на обработку аппаратных прерываний"""
     try:
         if platform.system() == "Linux":
             return get_irq_time_linux()
         elif platform.system() == "Windows":
             return get_irq_time_windows()
-        else:
-            return -1
     except Exception as e:
-        print(f"Ошибка при получении времени аппаратных прерываний: {e}")
+        print(f"Ошибка сбора параметра \"Процент времени CPU на обработку аппаратных прерываний\": \n{e}")
         return -1
 
 
 def get_irq_time_linux():
-    """Получает процент времени на аппаратные прерывания для Linux"""
+    """Возвращает процент времени на аппаратные прерывания для Linux"""
     with open("/proc/stat") as file:
         line = file.readline()
         fields = list(map(int, line.strip().split()[1:]))
         total_time = sum(fields)
         irq_time = fields[5]
-        return (irq_time / total_time) * 100 if total_time else -1
+        return (irq_time / total_time) * 100
 
 
 def get_irq_time_windows():
@@ -74,21 +92,18 @@ def get_irq_time_windows():
     # except Exception as e:
     #     print(f"Ошибка при получении IRQ Time через WMI: {e}")
     #     return -1
-    return -1
+    raise ValueError("На windows не реализован сбор параметра \"Процент времени CPU на обработку аппаратных прерываний\"")
 
 
 def get_cpu_time_soft_irq():
-    """Возвращает процент времени, которое центральный
-       процессор тратит на обработку программных прерываний"""
+    """Возвращает процент времени, которое центральный процессор тратит на обработку программных прерываний"""
     try:
         if platform.system() == "Linux":
             return get_softirq_time_linux()
         elif platform.system() == "Windows":
             return get_dpc_time_windows()
-        else:
-            return -1
     except Exception as e:
-        print(f"Ошибка при получении времени программных прерываний: {e}")
+        print(f"Ошибка сбора параметра \"Процент времени CPU на обработку программных прерываний\": \n{e}")
         return -1
 
 
@@ -98,8 +113,8 @@ def get_softirq_time_linux():
         line = file.readline()
         fields = list(map(int, line.strip().split()[1:]))
         total_time = sum(fields)
-        softirq_time = fields[7]  # softirq — восьмое поле в списке
-        return (softirq_time / total_time) * 100 if total_time else -1
+        softirq_time = fields[7]
+        return (softirq_time / total_time) * 100
 
 
 def get_dpc_time_windows():
@@ -113,36 +128,46 @@ def get_dpc_time_windows():
     # except Exception as e:
     #     print(f"Ошибка при получении DPC Time через WMI: {e}")
     #     return -1
-    return -1
+    raise ValueError("На windows не реализован сбор параметра \"Процент времени CPU на обработку программных прерываний\"")
 
 
 def get_cpu_time_idle():
-    """Возвращает процент времени, которое центральный
-       процессор простаивает"""
-    return psutil.cpu_times_percent(interval=1).idle or -1
+    """Возвращает процент времени, которое центральный процессор простаивает"""
+    try:
+        psutil.cpu_times_percent(interval=1)
+        return psutil.cpu_times_percent(interval=1).idle
+    except Exception as e:
+        print(f"Ошибка сбора параметра \"Процент времени простоя CPU\": \n{e}")
+        return -1
 
 
 def get_cpu_time_io_wait():
-    """Возвращает процент времени, которое центральный
-       процессор простаивает в ожидании результатов операций ввода/вывода"""
-    if platform.system() == "Linux":
-        return psutil.cpu_times_percent(interval=1).iowait or -1
-    elif platform.system() == "Windows":
-        # try:
-        #     c = wmi.WMI(namespace="root\\CIMV2")
-        #     processor_data = c.Win32_PerfFormattedData_Counters_ProcessorInformation()
-        #     for instance in processor_data:
-        #         if instance.Name == "_Total":
-        #             idle_time = float(instance.PercentIdleTime)
-        #             processor_time = float(instance.PercentProcessorTime)
-        #             # Формула: I/O Wait ~ Idle Time - Processor Time (приближение)
-        #             io_wait_time = max(idle_time - processor_time, 0)
-        #             return io_wait_time
-        # except Exception as e:
-        #     print(f"Ошибка при получении I/O Wait через WMI: {e}")
-        #     return -1
+    """Возвращает процент времени, которое центральный процессор простаивает в ожидании результатов операций ввода/вывода"""
+    try:
+        if platform.system() == "Linux":
+            return psutil.cpu_times_percent(interval=1).iowait
+        elif platform.system() == "Windows":
+            return get_cpu_time_io_wait_windows()
+    except Exception as e:
+        print(f"Ошибка сбора параметра \"Процент времени простоя CPU в ожидании результатов операций ввода/вывода\": \n{e}")
         return -1
-    return -1
+
+
+def get_cpu_time_io_wait_windows():
+    # try:
+    #     c = wmi.WMI(namespace="root\\CIMV2")
+    #     processor_data = c.Win32_PerfFormattedData_Counters_ProcessorInformation()
+    #     for instance in processor_data:
+    #         if instance.Name == "_Total":
+    #             idle_time = float(instance.PercentIdleTime)
+    #             processor_time = float(instance.PercentProcessorTime)
+    #             # Формула: I/O Wait ~ Idle Time - Processor Time (приближение)
+    #             io_wait_time = max(idle_time - processor_time, 0)
+    #             return io_wait_time
+    # except Exception as e:
+    #     print(f"Ошибка при получении I/O Wait через WMI: {e}")
+    #     return -1
+    raise ValueError("На windows не реализован сбор параметра \"Процент времени простоя CPU в ожидании результатов операций ввода/вывода\"")
 
 
 print(f"1. Физические ядра: {get_cpu_physical_core_count()}")
