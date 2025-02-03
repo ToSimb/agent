b0351a12f5a7e676a33b8c4b3bee3003b95afcfe
Smirnov <Smirnov>
2025-02-03 11:21:20 +0300

    cpu

--- monitoring/ram/ram.py
+++ monitoring/ram/ram.py
@@ -4,32 +4,56 @@ import psutil
 
 def get_virtual_memory_total():
     """Возвращает общее количество оперативной памяти в системе"""
-    return psutil.virtual_memory().total or -1
+    try:
+        return psutil.virtual_memory().total
+    except Exception as e:
+        print(f"Ошибка сбора параметра \"Общее количество оперативной памяти в системе\": \n{e}")
+        return -1
 
 
 def get_virtual_memory_used():
     """Возвращает количество занятой оперативной памяти в системе"""
-    return psutil.virtual_memory().used or -1
+    try:
+        return psutil.virtual_memory().used
+    except Exception as e:
+        print(f"Ошибка сбора параметра \"Количество занятой оперативной памяти в системе\": \n{e}")
+        return -1
 
 
 def get_virtual_memory_free():
     """Возвращает количество свободной оперативной памяти в системе"""
-    return psutil.virtual_memory().free or -1
+    try:
+        return psutil.virtual_memory().free
+    except Exception as e:
+        print(f"Ошибка сбора параметра \"Количество свобоной оперативной памяти в системе\": \n{e}")
+        return -1
 
 
 def get_swap_memory_total():
     """Возвращает общее количество swap-памяти в системе"""
-    return psutil.swap_memory().total or -1
+    try:
+        return psutil.swap_memory().total
+    except Exception as e:
+        print(f"Ошибка сбора параметра \"Общее количество swap-памяти в системе\": \n{e}")
+        return -1
 
 
 def get_swap_memory_used():
     """Возвращает количество занятой swap-памяти в системе"""
-    return psutil.swap_memory().used or -1
+    try:
+        return psutil.swap_memory().used
+    except Exception as e:
+        print(f"Ошибка сбора параметра \"Количество занятой swap-памяти в системе\": \n{e}")
+        return -1
 
 
 def get_swap_memory_free():
     """Возвращает количество свободной swap-памяти в системе"""
-    return psutil.swap_memory().free or -1
+    try:
+        return psutil.swap_memory().free
+    except Exception as e:
+        print(f"Ошибка сбора параметра \"Количество свободной swap-памяти в системе\": \n{e}")
+        return -1
 
 
 print(f"1. Общее количество оперативной памяти в системе: {get_virtual_memory_total()}")
