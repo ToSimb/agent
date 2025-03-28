5e94a5d7199bc2dc464f6692c3109b918438b272
tosimb <tosimb@yandex.ru>
2025-03-28 17:19:36 +0300

    Описаны логгеры

---
+++ logger/logger_monitoring.py
@@ -0,0 +1,18 @@
+import logging
+from logging.handlers import RotatingFileHandler
+
+def setup_logger():
+    logger = logging.getLogger('agent:monitoring')
+    logger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    file_handler = RotatingFileHandler("logger/logs_monitoring.log", maxBytes=300000, backupCount=5)
+    file_handler.setLevel(logging.DEBUG)
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.DEBUG)
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+    return logger
+
+logger_monitoring = setup_logger()
